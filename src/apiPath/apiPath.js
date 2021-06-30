50da31b7113e23a0cd55096334b119427d9674ef
lordu98 <u.makhmudov211326@gmail.com>
2021-06-30 11:25:18 +0500

    api path

---
+++ src/apiPath/apiPath.js
@@ -0,0 +1,36 @@
+export const apiPath={
+    // Users
+    addUser:'user/add',
+    editUser:'user/edit/',
+    deleteUser:'user/delete/',
+    allUsers:'user/all',
+    byIdUser:'user/',
+
+    // Mining System
+    addMining:'mining/add',
+    editMining:'mining/edit',
+    deleteMining:'mining/delete/',
+    allMinings:'mining/all',
+    byIdMining:'mining/',
+
+    // Uppg
+    addUppg:'uppg/add',
+    editUppg:'uppg/edit',
+    deleteUppg:'uppg/delete/',
+    allUppgs:'uppg/all',
+    byIdUppg:'uppg/',
+
+    // Collection Point
+    addCollection:'colection/add',
+    editCollection:'collection/edit',
+    deleteCollection:'collection/delete/',
+    allCollections:'collection/all',
+    byIdCollection:'collection/',
+
+    // Well
+    addWell:'well/add',
+    editWell:'well/edit',
+    deleteWell:'well/delete/',
+    allWells:'well/all',
+    byIdWell:'well/',
+}
---
+++ src/utills/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL_ADMIN='http://localhost:8080/api/admin/'
+export const TOKEN='ShurtanMSC JWT Token'
+export const ROLE='USER ROLE'
