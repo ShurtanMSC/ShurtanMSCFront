ba1d1c289262125d5594fb883c36068f7af5df38
UtkirMansurovich <81430269+UtkirMansurovich@users.noreply.github.com>
2021-08-16 14:23:42 +0500

    type bugs

--- src/requests/Test.js
+++ src/requests/Test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import SuperAdminRequests from "./SuperAdminRequests";
 
 function Test(props) {
-    function testRequests() {
-        let data = {
-            gasCompositionId: 17,
-            miningSystemId: 1,
-            molarFraction: 99,
-            id:16
-        }
+    // function testRequests() {
+    //     let data = {
+    //         gasCompositionId: 17,
+    //         miningSystemId: 1,
+    //         molarFraction: 99,
+    //         id:16
+    //     }
 
         SuperAdminRequests.deleteMiningSystemGasComposition(16)
             .then(res =>
