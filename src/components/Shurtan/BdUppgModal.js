b7b9c5649b2f6238740b8dc9db8e1230112002c8
UtkirMansurovich <utkir_xan@mail.ru>
2022-07-04 17:43:21 +0500

    correct syntaxt BdUppgModal.js

--- src/components/Shurtan/BdUppgModal.js
+++ src/components/Shurtan/BdUppgModal.js
@@ -116,7 +116,7 @@ const BtnSearchModal = ({showBdUppgModal, setShowBdUppgModal}) => {
                                 <Tr>
                                     <Td>{uppgDatabase&&uppgDatabase[1] ?
                                       // (Math.round(uppgDatabase[1].nakoplenniy_obyom*100)/100)
-                                      (Math.round(uppgDatabase[1].nakoplenniy_obyom_za_pered_mesyach*100)/100)*(new Date().getMonth())+(Math.round(uppgDatabase[1].nakoplenniy_obyom_za_pered_mesyach*100)/100)
+                                      (Math.round(uppgDatabase[1].nakoplenniy_obyom_za_pered_mesyach*new Date().getMonth()*100)/100)+(Math.round(uppgDatabase[1].nakoplenniy_obyom_za_pered_mesyach*100)/100)
                                       : 0}
                                     </Td>
                                     <Td>{uppgDatabase&&uppgDatabase[1] ? (Math.round(uppgDatabase[1].nakoplenniy_obyom_za_pered_mesyach*100)/100) : 0}</Td>
